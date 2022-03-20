d1a39133456ea3d4da008fc6de5108eb710c9ae8
hejonathan <68175370+hejonathan@users.noreply.github.com>
2022-03-20 15:38:49 -0500

    new branch: added data logger to easily visualize flight data

--- data_logger.py
+++ data_logger.py
@@ -11,11 +11,23 @@ class logger:
         self.drone_d.append(d)
         self.drone_v.append(v)
         self.drone_a.append(a)
+        if len(self.drone_d) > MAX_SIZE:
+            self.drone_d.pop(0)
+        if len(self.drone_v) > MAX_SIZE:
+            self.drone_v.pop(0)
+        if len(self.drone_a) > MAX_SIZE:
+            self.drone_d.pop(0)
 
     def update_obj(self, d: tuple, v: tuple, a: tuple):
         self.obj_d.append(d)
         self.obj_v.append(v)
         self.obj_a.append(a)
+        if len(self.obj_d) > MAX_SIZE:
+            self.obj_d.pop(0)
+        if len(self.obj_v) > MAX_SIZE:
+            self.obj_v.pop(0)
+        if len(self.obj_a) > MAX_SIZE:
+            self.obj_a.pop(0)
 
     def graph_drone(self):
         pass
