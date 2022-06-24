bcb2ee5fd977b055bb51f4cb3ad0e0893c7deece
D-Belinda <belinda.dong@gmail.com>
2022-06-23 23:17:11 -0500

    Code to save coords

--- record-video-display.py
+++ record-video-display.py
@@ -1,26 +1,42 @@
 import time, cv2
 from threading import Thread
+import numpy as np
 from djitellopy import Tello
-import time
+from object_tracking_class import object_tracker
+
+DISTANCE = 10   # Distance (cm) the drone moves per command
 
 tello = Tello()
 
 tello.connect(False)
 
 keepRecording = True
+tello.streamoff()   # Call just in case
 tello.streamon()
-frame_read = tello.get_frame_read()
+
+track = object_tracker()
+frame = tello.get_frame_read().frame
+framecenter = (frame.shape[1] / 2, frame.shape[0] / 2)
+print(framecenter)
+
+offset_coords = []  # Empty array to store all sets of coordinates
 
 def videoRecorder():
-    # create a VideoWrite object, recording to ./video.avi
-    while True:
-        # In reality you want to display frames in a separate thread. Otherwise
-        #  they will freeze while the drone moves.
-        img = frame_read.frame
-        img = cv2.resize(img, (480,360))
-        cv2.imshow("drone", img)
 
-        # Movement, not used right now
+    while True:
+        # grab the current frame
+        frame = tello.get_frame_read().frame
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+        # The frame was also resized in the tello.py BackgroundFrameRead source code
+        # Need to ensure center is matching, format: (width, height)
+        frame = cv2.resize(frame, (640, 480))
+        frame = track.processAll(frame, track.hsv_value, framecenter)
+
+        # show the frame to the screen
+        cv2.imshow("Frame", frame)
+
+        # Discrete movement
         key = cv2.waitKey(1) & 0xff
         if key == 27:  # ESC
             break
@@ -31,38 +47,42 @@ def videoRecorder():
             tello.land()
         # Directions
         elif key == ord('w'):
-            tello.move_forward(30)
+            tello.move_forward(DISTANCE)
         elif key == ord('s'):
-            tello.move_back(30)
+            tello.move_back(DISTANCE)
         elif key == ord('a'):
-            tello.move_left(30)
+            tello.move_left(DISTANCE)
         elif key == ord('d'):
-            tello.move_right(30)
+            tello.move_right(DISTANCE)
         # Rotations
         elif key == ord('l'):
-            tello.rotate_clockwise(30)
+            tello.rotate_clockwise(DISTANCE)
         elif key == ord('j'):
-            tello.rotate_counter_clockwise(30)
+            tello.rotate_counter_clockwise(DISTANCE)
         # Elevation
         elif key == ord('i'):
-            tello.move_up(30)
+            tello.move_up(DISTANCE)
         elif key == ord('k'):
-            tello.move_down(30)
+            tello.move_down(DISTANCE)
+        # Recording data
+        elif key == ord('r'):
+            offset = track.getOffset()
+            print(offset)
 
+            offset_coords.append(offset)
 
 # we need to run the recorder in a separate thread, otherwise blocking options
 #  would prevent frames from getting added to the video
 recorder = Thread(target=videoRecorder)
 recorder.start()
 
-#tello.takeoff()
-#tello.move_up(100)
-#tello.rotate_counter_clockwise(360)
-#tello.land()
-#time.sleep(30);
-
-
 keepRecording = False
 recorder.join()
+
+# save data about object's coordinates and size into 'coordinates.csv'
+# this file will later be imported directly into MATLAB and graphed
+with open('coordinates.csv', 'w') as csvfile:
+    np.savetxt(csvfile, offset_coords)
+    print('Coordinates saved')
 
 cv2.destroyAllWindows()
